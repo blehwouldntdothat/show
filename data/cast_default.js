a2a3d2661e06a06fb177d7a0bd6f4674b0992641
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-03-15 11:22:16 +0000

    Create cast_default.js

---
+++ data/cast_default.js
@@ -0,0 +1,26 @@
+[
+  {
+    "id": 1,
+    "name": "Alex",
+    "image": "images/alex.png",
+    "team": "Screaming Gophers",
+    "stats": {
+      "challenge": 7,
+      "strategy": 6,
+      "social": 5,
+      "chaos": 3
+    }
+  },
+  {
+    "id": 2,
+    "name": "Blake",
+    "image": "images/blake.png",
+    "team": "Killer Bass",
+    "stats": {
+      "challenge": 4,
+      "strategy": 8,
+      "social": 7,
+      "chaos": 6
+    }
+  }
+]
